b1ed1b894acd07413f94dd447447136db09b5644
hyunsik2000 <ryuhsik2000@naver.com>
2024-03-27 19:51:56 +0900

    :white_check_mark: 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import Button from 'react-bootstrap/Button';
-import logo from './logo.svg';
+import {Container, Nav, Navbar, Carousel, Image } from 'react-bootstrap';
+import { useState } from "react";
 import './App.css';
+import data from './data.js';
 
 function App() {
+
+  let [newjeans] = useState(data)
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
-
+      <Navbar bg="dark" data-bs-theme="dark" className='custom-navbar'>
+        <Container>
+          <Navbar.Brand href="#home">뉴진스</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#album">Album</Nav.Link>
+            <Nav.Link href="#member">Member</Nav.Link>
+            <Nav.Link href="#more">More</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div style = {{height: 1}}/>
       <div className="main-bg"></div>
+      <UncontrolledExample/>
     </div>
+  );
+}
+
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <Image src='/1번 앨범.jpg' className='carousel-image'/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src='/2번 앨범.jpg' className='carousel-image'/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src='/3번앨범.jpg' className='carousel-image'/>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,39 @@
+let data = 
+[
+    {
+      id : 0,
+      title : "혜인",
+      content : "2004.05.07(19세)",
+      born : "한국"
+    },
+  
+    {
+      id : 1,
+      title : "민지",
+      content : "2004. 10. 06. (19세)",
+      born : "한국" 
+    },
+  
+    {
+      id : 2,
+      title : "해린",
+      content : "2005. 04. 11. (18세)",
+      born : "한국"
+    },
+
+    {
+        id : 3,
+        title : "다니엘",
+        content : "2006. 05. 15. (17세)",
+        born : "호주 한국 혼혈"
+      },
+      {
+        id : 4,
+        title : "하니",
+        content : "2008. 04. 21. (15세)",
+        born : "호주 베트남 혼혈"
+      }
+  ] 
+
+
+  export default data;
